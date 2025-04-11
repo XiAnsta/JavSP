f4d25cc1f91e51f5a1df97549c09d962bdf6f514
XiAnsta <75986549+XiAnsta@users.noreply.github.com>
2025-04-11 19:17:22 +0000

    fix 缺少收录时间导致异常

--- javsp/web/fc2ppvdb.py
+++ javsp/web/fc2ppvdb.py
@@ -47,7 +47,11 @@ def parse_data(movie: MovieInfo):
     movie.title = get_list_first(title)
     movie.genre = genre
     movie.actress = actress
-    movie.duration = str(strftime_to_minutes(get_list_first(duration_str)))
+    #movie.duration = str(strftime_to_minutes(get_list_first(duration_str)))
+    movie.duration = "30"
+    if duration_str and len(duration_str) > 0:
+        movie.duration = str(strftime_to_minutes(duration_str[0]))
+
     movie.publish_date = get_list_first(publish_date)
     movie.publisher = get_list_first(publisher)
     movie.uncensored = uncensored
